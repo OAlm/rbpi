a7865c5b9ed8ce7b88090e0d7a189ca820ecd5ec
Nikita Korhonen <nikitak@metropolia.fi>
2017-05-16 13:54:58 +0300

    simple

--- app.js
+++ app.js
@@ -1,40 +1,14 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
+var uws = require('uws');
+var uwsClient = new uws("ws://195.148.149.148:3001", {perMessageDeflate: false});
 
 
-var app = express();
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+uwsClient.on('open', function open() {
+    uwsClient.send('rbpi Initialization');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+uwsClient.on('message', function incoming(data, flags) {
+    // flags.binary will be set if a binary data is received.
+    // flags.masked will be set if the data was masked.
+    console.log("Data /", data);
+    console.log("Flags /", flags);
 });
-
-module.exports = app;
